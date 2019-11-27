3a4fccdea7a7fa05f6c2c7dc922040982092b2fa
Ansela Bognandi Refsbäck <ansela@MacBook-Pro.lan>
2019-11-27 12:23:54 +0100

    update my menu item to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+//en slags funktion som ger tillbaka en powered up component
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -17,4 +22,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+//gör så att jag får tillgång till history props i menuItem
+export default withRouter(MenuItem);
