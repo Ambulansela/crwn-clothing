8970b33352610a56c19f2e6b83ba6e99e8185456
Ansela Bognandi Refsbäck <ansela@MacBook-Pro.lan>
2019-11-28 11:36:33 +0100

    updating our app with a shop page, collection preview component and collection item component

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./collection-item.styles.scss";
+
+//behöver ingen state, så gör ingen class, utan arrow function component
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
